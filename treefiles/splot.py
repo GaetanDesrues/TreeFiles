e35fdcbb6cbdc0cf3c826d4c5ce502672681cc4e
gaetan <gaetan.desrues@inria.fr>
2021-03-10 20:40:52 +0100

    fix splot

--- treefiles/splot.py
+++ treefiles/splot.py
@@ -3,20 +3,24 @@ import matplotlib.pyplot as plt
 
 class SimplePlot:
     def __init__(self, fname=None, show=True, save=None):
-        self.fname = fname
-        self.save = fname is not None if save is None else save
-        self.show = show
+        self._fname = fname
+        self._save = fname is not None if save is None else save
+        self._show = show
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         fig = plt.gcf()
 
-        if self.fname is not None and self.save:
-            fig.savefig(self.fname)
+        if self._fname is not None and self._save:
+            fig.savefig(self._fname)
 
-        if self.show:
+        if self._show:
             plt.show()
 
         plt.close(fig)
+
+    def save(self, fname):
+        self._fname = fname
+        self._save = True
