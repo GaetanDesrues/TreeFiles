5cae2f4b81061e6c5de146997c77f2b58bee86ec
gaetan <gaetan.desrues@inria.fr>
2021-04-28 16:41:30 +0200

    update treefile package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="treefiles",
-    version="0.1.043",
+    version="0.1.044",
     author="Gaetan Desrues",
     author_email="gdesrues@gmail.com",
     description="Description",
