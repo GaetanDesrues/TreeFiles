c23824bd2f2c5e6cd499e96d4ff56b6d04c338e8
gaetan <gaetan.desrues@inria.fr>
2021-06-08 12:15:26 +0200

    FIX: mails no keyring

--- treefiles/mails.py
+++ treefiles/mails.py
@@ -1,4 +1,5 @@
 import logging
+import os.path
 import smtplib
 import ssl
 from email.message import Message
@@ -10,15 +11,23 @@ def register_treemails(uname: str, passwd: str):
     keyring.set_password("treemails", uname, passwd)
 
 
-def send_mail(login: str, msg: Message, port: int = 465):
-    import keyring
+def send_mail(login: str, msg: Message, port: int = 465, passwd: str = None):
+    if passwd is None:
+        import keyring
+
+        logging.getLogger("keyring").setLevel(logging.ERROR)
+
+        passwd = keyring.get_password("treemails", login)
+    else:
+        if os.path.isfile(passwd):
+            with open(passwd) as f:
+                passwd = f.read().strip()
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(login, keyring.get_password("treemails", login))
+        server.login(login, passwd)
         server.send_message(msg)
     log.info(f"Message sent to {msg['To']!r}")
 
 
-logging.getLogger("keyring").setLevel(logging.ERROR)
 log = logging.getLogger(__name__)
