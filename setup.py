c6f5dc6b7c9b793a5184681604bef40cb9010fff
Gaetan Desrues <gaetan.desrues@inria.fr>
2022-09-06 20:22:16 +0200

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="treefiles",
-    version="0.1.164",
+    version="0.1.165",
     author="Gaetan Desrues",
     author_email="gdesrues@gmail.com",
     description="Description",
